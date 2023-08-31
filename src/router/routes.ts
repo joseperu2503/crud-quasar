ab7aff16d8a44284784a76b8b5d55af0be924a7e
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-08-30 20:42:12 -0500

    Update routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,11 @@
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-  // {
-  //   path: '/',
-  //   component: () => import('layouts/MainLayout.vue'),
-  //   children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  // },
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '/dashboard', component: () => import('pages/IndexPage.vue') }],
+  },
   {
     path: '/',
     redirect: '/login'
