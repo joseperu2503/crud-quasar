282d503fd45c87d654172fbdfb64e7da1f492cbc
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-09-04 00:40:01 -0500

    Update useStatusBar.ts

--- src/composables/useStatusBar.ts
+++ src/composables/useStatusBar.ts
@@ -8,7 +8,6 @@ export function useStatusBar() {
   // $q.addressbarColor.set('#1976D2')
 
   const setBackgroundColorWhite = async () => {
-    console.log('setBackgroundColorWhite')
     if ($q.platform.is.nativeMobile) {
       StatusBar.setBackgroundColor({ color: '#FFFFFF' });
       await StatusBar.setStyle({ style: Style.Light })
@@ -16,8 +15,6 @@ export function useStatusBar() {
   }
 
   const setBackgroundColorPrimary = async () => {
-    console.log('setBackgroundColorPrimary')
-
     if ($q.platform.is.nativeMobile) {
       StatusBar.setBackgroundColor({ color: '#1976D2' });
       await StatusBar.setStyle({ style: Style.Dark })
