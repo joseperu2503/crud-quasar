8e304646033663a22a18377d3cc9c59687d30a49
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-09-17 23:20:26 -0500

    mejora de my products

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -1,11 +1,17 @@
 export interface Product {
-  created_at: string;
-  name: string;
   id: number;
+  name: string;
+  description: string;
   price: number;
   stock: number;
   images: string[];
-  updated_at: string;
+  brand_id: number;
+  category_id: number;
+  colors: string[];
+  sizes: number[];
+  genders: any[];
+  free_shipping: boolean;
+  created_at: string;
 }
 
 export interface ProductErrors {
@@ -13,11 +19,44 @@ export interface ProductErrors {
   price?: string[];
   stock?: string[];
   images?: string[];
+  description?: string[];
 }
 
 export interface ProductForm {
   name: string | null;
+  description: string | null;
   price: number | null;
   stock: number | null;
   images: string[];
+  colors: string[];
+  genders: number[];
+  sizes: number[];
+  brand_id: number | null;
+  category_id: number | null;
+  free_shipping: boolean;
+}
+
+export interface ProductFormDataResponse {
+  brands: Brand[];
+  categories: Category[];
+  genders: Gender[];
+  sizes: Size[];
+}
+
+export interface Gender {
+  id: number;
+  name: string;
+}
+
+export interface Size {
+  id: number;
+  name: string;
+}
+export interface Category {
+  id: number;
+  name: string;
+}
+export interface Brand {
+  id: number;
+  name: string;
 }
--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,20 +1,63 @@
 <template>
   <q-page class="max-w-6xl mx-auto py-16 w-11/12">
-    <div class="grid-pruducts justify-center gap-4">
-      <q-card class="w-full " v-for="(product, index) in products" :key="index">
-        <q-img :src="product.images[0]" height="300" class="cursor-pointer" @click="editProduct(product.id)" />
-        <q-card-section>
-          <div class="text-gray-600 font-semibold text-md cursor-pointer hover:underline truncate"
-            @click="editProduct(product.id)">
-            {{ product.name }}
-          </div>
-        </q-card-section>
-      </q-card>
+
+    <div class="flex justify-between">
+      <div class="text-h6">My products</div>
+      <q-btn :icon="isGridView ? 'grid_view' : 'toc'" flat round @click="isGridView = !isGridView" />
     </div>
-    <q-page-sticky position="bottom-right" :offset="[18, 18]">
-      <q-btn fab icon="add" color="primary" @click="newProduct" />
-    </q-page-sticky>
+    <template v-if="!isGridView">
+      <q-btn color="primary" icon="add" no-caps @click="newProduct" class="mt-4 mb-8">Create product</q-btn>
+      <q-table :rows="products" row-key="id" :loading="loading" hide-pagination flat
+        :rows-per-page-options="[products.length]">
+        <template v-slot:header="props">
+          <q-tr :props="props">
+            <q-th>ID</q-th>
+            <q-th>Name</q-th>
+            <q-th>Price</q-th>
+            <q-th>Stock</q-th>
+            <q-th>Date</q-th>
+            <q-th>Actions</q-th>
+          </q-tr>
+        </template>
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-th> {{ props.row.id }}</q-th>
+            <q-th> {{ props.row.name }}</q-th>
+            <q-th> {{ props.row.price }}</q-th>
+            <q-th> {{ props.row.stock }}</q-th>
+            <q-th> {{ props.row.created_at }}</q-th>
+            <q-th> </q-th>
+          </q-tr>
+        </template>
+      </q-table>
+    </template>
+    <template v-else>
+      <div class="flex justify-center mt-6">
+        <q-pagination v-model="pagination.currentPage" :max="pagination.totalPages" boundary-numbers :max-pages="6"
+          @update:modelValue="getProducts" direction-links />
+      </div>
+      <div class="grid-products justify-center gap-4 mt-8">
+        <q-card class="w-full " v-for="(product, index) in products" :key="index">
+          <q-img :src="product.images[0]" height="300" class="cursor-pointer" @click="editProduct(product.id)" />
+          <q-card-section>
+            <div class="text-gray-600 font-semibold text-md cursor-pointer hover:underline truncate"
+              @click="editProduct(product.id)">
+              {{ product.name }}
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn fab icon="add" color="primary" @click="newProduct" />
+      </q-page-sticky>
+    </template>
+
+    <div class="flex justify-center mt-6">
+      <q-pagination v-model="pagination.currentPage" :max="pagination.totalPages" boundary-numbers :max-pages="6"
+        @update:modelValue="getProducts" direction-links />
+    </div>
     <ProductForm v-model:showModal="showModal" :productId="productId" @reloadData="getProducts" />
+
   </q-page>
 </template>
 
@@ -23,13 +66,16 @@ import { ref } from 'vue';
 import { appApi } from '@/api/appApi'
 import { Product } from '@/interfaces/product.interface'
 import ProductForm from '@/components/ProductForm.vue';
+import { ResponsePaginate } from '@/interfaces/responsePaginate.interface';
 
 const products = ref<Product[]>([])
-
 const showModal = ref(false)
 const loading = ref(false)
 const productId = ref<number | null>(null)
-
+const pagination = ref({
+  currentPage: 1,
+  totalPages: 10,
+})
 const newProduct = () => {
   productId.value = null
   showModal.value = true
@@ -38,8 +84,17 @@ const newProduct = () => {
 const getProducts = async () => {
   loading.value = true
   try {
-    let response = await appApi.get("/products")
-    products.value = response.data
+    const response = await appApi.get<ResponsePaginate<Product>>("/my-products", {
+      params: {
+        page: pagination.value.currentPage
+      }
+    })
+    const myProductsResponse = response.data
+    products.value = myProductsResponse.data
+    pagination.value = {
+      currentPage: myProductsResponse.meta.current_page,
+      totalPages: myProductsResponse.meta.last_page
+    }
   } catch (error) {
   }
   loading.value = false
@@ -50,12 +105,13 @@ const editProduct = (id: number) => {
   showModal.value = true
 }
 
+const isGridView = ref(false)
 
 getProducts()
 
 </script>
 <style>
-.grid-pruducts {
+.grid-products {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(280px, 300px));
 }
